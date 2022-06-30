050866583235d33f0c9bbafd542cf82a49a08906
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-06-30 09:30:42 +0700

    create user model controller routes

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-// Routes
-
-const app = express();
-
-// Middleware
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoute from './routers/authRoute.js';
+import userRoute from './routers/userRoute.js';
 
 dotenv.config();
+const app = express();
 
-mongoose
-  .connect(process.env.MONGO_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening at ${process.env.PORT}`)
-    );
-  })
-  .catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_DB, () => {
+    console.log('Connected to Mongo DB');
+});
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//Routes
+app.use('/auth', authRoute);
+app.use('/user', userRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log('server is running');
+});
---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        admin: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    { timestamps: true },
+);
+
+const UserModel = mongoose.model('Users', userSchema);
+export default UserModel;

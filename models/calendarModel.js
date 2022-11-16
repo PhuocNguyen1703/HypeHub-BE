9b0cb043f4980d4a902b3036a94fe1aced840767
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-11-16 19:58:18 +0700

    add calendarModel

---
+++ models/calendarModel.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+const CalendarSchema = mongoose.Schema(
+    {
+        userId: {
+            type: String,
+        },
+        title: {
+            type: String,
+        },
+        calendarType: {
+            type: String,
+        },
+        Date: {
+            type: String,
+        },
+        startTime: {
+            type: String,
+        },
+        endTime: {
+            type: String,
+        },
+        content: {
+            type: String,
+        },
+        theme: {
+            type: String,
+        },
+        completed: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const CalendarModel = mongoose.model('Calendar', CalendarSchema);
+export default CalendarModel;
--- models/emailModel.js
+++ models/emailModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const EmailSchema = mongoose.Schema(
     {
-        senderID: {
+        senderId: {
             type: String,
         },
         receiverId: {

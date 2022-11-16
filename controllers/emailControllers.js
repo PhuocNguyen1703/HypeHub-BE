c3b07d65f1d369dd25976f3a8cce084c26979765
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-11-16 19:37:05 +0700

    complete email controller

--- controllers/emailControllers.js
+++ controllers/emailControllers.js
@@ -11,6 +11,24 @@ export const createEmail = async (req, res) => {
     }
 };
 
-export const getAllEmails = async (req, res) => {
+export const getReceiveEmails = async (req, res) => {
+    const { receiverId } = req.params;
 
+    try {
+        const result = await EmailModel.find({ receiverId });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const getSenderEmails = async (req, res) => {
+    const { senderId } = req.params;
+
+    try {
+        const result = await EmailModel.find({ senderId });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };

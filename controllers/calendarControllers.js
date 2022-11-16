bd717f02fdb0ce2ffa63ddd8cb23166026ba292e
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-11-16 20:04:09 +0700

    add calendar Controllers

---
+++ controllers/calendarControllers.js
@@ -0,0 +1,23 @@
+import CalendarModel from '../models/calendarModel.js';
+
+export const createCalendar = async (req, res) => {
+    const newCalendar = new CalendarModel(req.body);
+
+    try {
+        const result = await newCalendar.save();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const getAllCalendar = async (req, res) => {
+    const { userId } = req.body;
+
+    try {
+        const result = await CalendarModel.find({ userId });
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};

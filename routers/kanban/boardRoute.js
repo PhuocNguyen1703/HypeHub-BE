fad61ac799a33cf4d5910339cdf32755d6e3c6e9
phuocnguyen1703 <nguyenquocphuocit@gmail.com>
2022-12-21 22:05:04 +0700

    update boardRoute

--- routers/kanban/boardRoute.js
+++ routers/kanban/boardRoute.js
@@ -4,14 +4,6 @@ import { boardValidation } from '../../validations/kanban/boardValidation.js';
 
 const router = express.Router();
 
-// router.post('/', createSection);
-// router.get('/:userId', getAllSection);
-// router.put('/:sectionId', updateSection);
-// router.delete('/:sectionId', deleteSection);
-
-// router.post('/task', createTask);
-// router.get('/task/:sectionId', getAllTask);
-
 router.post('/', boardValidation.createNew, boardController.createNew);
 router.get('/:id', boardController.getAllBoard);
 router.put('/:id', boardValidation.update, boardController.update);
